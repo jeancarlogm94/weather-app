ee4914d88a6fbf329849b2c18d4266b882d1e722
John Doe <johndoe@example.com>
2022-06-20 16:35:30 -0500

    change style

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,67 +2,67 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Weather = () => {
+  const [location, setLocation] = useState();
+  const [degrees, setDegrees] = useState();
+  const [isDegrees, setIsDegrees] = useState(true);
 
-    const [ location, setLocation ] = useState()
-    const [ degrees, setDegrees] = useState()
-    const [ isDegrees, setIsDegrees ] = useState(true)
-    
-    useEffect(() => {
-        const geolocation = (position) => {
-            // console.log(position)
-            const latitude = position.coords.latitude
-            const longitude = position.coords.longitude
+  useEffect(() => {
+    const geolocation = (position) => {
+      // console.log(position)
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
 
-            const weatherApi =  `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=69dd21e8c81f43762aebbf2dd452bfa9`
-    
-            axios.get(weatherApi)
-            .then(res => {
-                setLocation(res.data)
-                setDegrees(Math.trunc((res.data.main.temp) -273.15))
-            })
-        }
+      const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=9e40f0c1a5900c65acf188e03f200692`;
 
-        const error = (error) => {
-            console.log(error)
-        }
+      axios.get(weatherApi).then((res) => {
+        setLocation(res.data);
+        setDegrees(Math.trunc(res.data.main.temp - 273.15));
+      });
+    };
 
-        navigator.geolocation.getCurrentPosition(geolocation, error)
-    }, [])
+    const error = (error) => {
+      console.log(error);
+    };
 
-        const degreesCelsius = Math.trunc((location?.main?.temp) - 273.15)
-        const degreesFahrenheit = ((degrees * 9/5) + 32)
-    
-        const urlIcon = location?.weather?.[0].icon
-        const icon = `http://openweathermap.org/img/wn/${urlIcon}.png`
+    navigator.geolocation.getCurrentPosition(geolocation, error);
+  }, []);
 
-        const changeDegrees = () => {
-            if (isDegrees) {
-                setDegrees(degreesFahrenheit)
-                setIsDegrees(false)
-            } else {
-                setDegrees(degreesCelsius)
-                setIsDegrees(true)
-            }
-        }
+  const degreesCelsius = Math.trunc(location?.main?.temp - 273.15);
+  const degreesFahrenheit = (degrees * 9) / 5 + 32;
 
-        console.log(location?.weather?.[0].main)
+  const urlIcon = location?.weather?.[0].icon;
+  const icon = `http://openweathermap.org/img/wn/${urlIcon}.png`;
 
-    return (
-        <div className='container'>
-            <h1>WEATHER APP</h1>
-            <img src={icon} alt="icon weather"/>
-            <h2>{degrees} {isDegrees ? '°C' : '°F'}</h2>
-            <h3>{location?.weather?.[0].main}</h3>
-            {/* <h3>{location?.weather?.[0].description}</h3> */}
-            <p></p>
-            <p>{location?.name}, {location?.sys?.country}</p>
-            <div className='icons'>
-            <button onClick={changeDegrees}>
-                Change Units
-            </button>
-            </div>
-        </div>
-    );
+  const changeDegrees = () => {
+    if (isDegrees) {
+      setDegrees(degreesFahrenheit);
+      setIsDegrees(false);
+    } else {
+      setDegrees(degreesCelsius);
+      setIsDegrees(true);
+    }
+  };
+
+  console.log(location?.weather?.[0].main);
+
+  return (
+    <div className="container">
+      <h1>WEATHER APP</h1>
+      <img src={icon} alt="icon weather" />
+      <h2>
+        {degrees} {isDegrees ? '°C' : '°F'}
+      </h2>
+      <h3>{location?.weather?.[0].main}</h3>
+      {/* <h3>{location?.weather?.[0].description}</h3> */}
+      <p></p>
+      <p>
+        {location?.name}, {location?.sys?.country}
+      </p>
+      <div className="icons">
+        <button onClick={changeDegrees}>Change Units</button>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
